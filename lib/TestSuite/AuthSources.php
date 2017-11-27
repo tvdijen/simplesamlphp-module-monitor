06d9842e2b23a55e4130e1f23a29988b7cb6b6d3
Tim van Dijen <tvdijen@gmail.com>
2017-11-27 12:56:12 +0100

    Test ldap-configuration on negotiate authsource

--- lib/TestSuite/AuthSources.php
+++ lib/TestSuite/AuthSources.php
@@ -25,11 +25,36 @@ final class sspmod_monitor_TestSuite_AuthSources extends sspmod_monitor_TestSuit
             switch ($authsource_data[0]) {
                 case 'ldap:LDAP':
                     $test = new sspmod_monitor_TestSuite_AuthSource_Ldap($monitor, array('authsource_id' => $authsource_id, 'authsource_data' => $authsource_data));
+                    $this->addTest($test);
                     $this->addMessages($test->getMessages(), $authsource_id);
                     break;
                 case 'negotiate:Negotiate':
                     $test = new sspmod_monitor_TestSuite_AuthSource_Negotiate($monitor, array('authsource_id' => $authsource_id, 'authsource_data' => $authsource_data));
+                    $this->addTest($test);
                     $this->addMessages($test->getMessages(), $authsource_id);
+
+                    // Prep authsource data
+                    if (isSet($authsource_data['debugLDAP'])) {
+                        $authsource_data['debug'] = $authsource_data['debugLDAP'];
+                        unset($authsource_data['debugLDAP']);
+                    }
+                    if (isSet($authsource_data['adminUser'])) {
+                        $authsource_data['search.username'] = $authsource_data['adminUser'];
+                        unset($authsource_data['adminUser']);
+                    }
+                    if (isSet($authsource_data['adminPassword'])) {
+                        $authsource_data['search.password'] = $authsource_data['adminPassword'];
+                        unset($authsource_data['adminPassword']);
+                    }
+                    if (isSet($authsource_data['base'])) {
+                        $authsource_data['search.base'] = $authsource_data['base'];
+                        unset($authsource_data['base']);
+                    }
+
+                    $ldap_test = new sspmod_monitor_TestSuite_AuthSource_Ldap($monitor, array('authsource_id' => $authsource_id, 'authsource_data' => $authsource_data));
+                    $this->addTest($ldap_test);
+                    $this->addMessages($ldap_test->getMessages(), $authsource_id);
+
                     break;
                 case 'multiauth:MultiAuth':
                     // Relies on other authsources
@@ -38,7 +63,6 @@ final class sspmod_monitor_TestSuite_AuthSources extends sspmod_monitor_TestSuit
                     // Not implemented
                     continue 2;
             }
-            $this->addTest($test);
         }
 
         parent::invokeTestSuite();
