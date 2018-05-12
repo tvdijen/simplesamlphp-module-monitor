81ee12a47f2e11fcc47cf480e70a7dfc9bf2f344
Tim van Dijen <tvdijen@gmail.com>
2018-05-12 22:50:12 +0200

    Add LDAP bind tests

---
+++ tests/lib/TestCase/LdapBindTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace SimpleSAML\Module\monitor\Test;
+
+use \SimpleSAML\Module\monitor\TestCase as TestCase;
+use \SimpleSAML\Module\monitor\TestData as TestData;
+use \SimpleSAML\Module\monitor\State as State;
+
+/**
+ * Tests for TestCase\Ldap\Configuration
+ */
+class TestLdapBindTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBindSuccesful()
+    {
+        $authSourceData = [
+            'search.username' => 'testuser',
+            'search.password' => 'password',
+        ];
+        $connectionMock = $this->getMock('LdapConnection', array('bind'));
+        $connectionMock->expects($this->once())->method('bind')->will($this->returnValue(true));
+        $confTest = new TestCase\AuthSource\Ldap\Bind(
+            new TestData([
+                'authSourceData' => $authSourceData,
+                'connection' => $connectionMock,
+            ])
+        );
+        $testResult = $confTest->getTestResult();
+
+        $this->assertEquals(State::OK, $testResult->getState());
+    }
+
+    public function testBindFailed()
+    {
+        $authSourceData = [
+            'search.username' => 'testuser',
+            'search.password' => 'password',
+        ];
+        $connectionMock = $this->getMock('LdapConnection', array('bind'));
+        $connectionMock->expects($this->once())->method('bind')->will($this->throwException(new \Exception()));
+        $confTest = new TestCase\AuthSource\Ldap\Bind(
+            new TestData([
+                'authSourceData' => $authSourceData,
+                'connection' => $connectionMock,
+            ])
+        );
+        $testResult = $confTest->getTestResult();
+
+        $this->assertEquals(State::FATAL, $testResult->getState());
+    }
+
+    public function testAuthenticationFailed()
+    {
+        $authSourceData = [
+            'search.username' => 'testuser',
+            'search.password' => 'password',
+        ];
+        $connectionMock = $this->getMock('LdapConnection', array('bind'));
+        $connectionMock->expects($this->once())->method('bind')->will($this->returnValue(false));
+        $confTest = new TestCase\AuthSource\Ldap\Bind(
+            new TestData([
+                'authSourceData' => $authSourceData,
+                'connection' => $connectionMock,
+            ])
+        );
+        $testResult = $confTest->getTestResult();
+
+        $this->assertEquals(State::ERROR, $testResult->getState());
+    }
+}
