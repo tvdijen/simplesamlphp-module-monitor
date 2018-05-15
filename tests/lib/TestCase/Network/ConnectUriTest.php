f355f8a232be0444cb45cb76f55370effd1f4786
Tim van Dijen <tvdijen@gmail.com>
2018-05-15 20:21:15 +0200

    Create webserver and add tests for TestCase/Network/ConnectUri

---
+++ tests/lib/TestCase/Network/ConnectUriTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SimpleSAML\Module\monitor\Test;
+
+use \SimpleSAML\Module\monitor\TestCase as TestCase;
+use \SimpleSAML\Module\monitor\TestData as TestData;
+use \SimpleSAML\Module\monitor\State as State;
+
+/**
+ * Tests for TestCase\Network\ConnectUri
+ */
+class TestConnectUriTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConnectUriOK()
+    {
+        $testData = new TestData([
+            'uri' => 'ssl://127.0.0.1:443',
+        ]);
+        $connectionTest = new TestCase\Network\ConnectUri($testData);
+        $testResult = $connectionTest->getTestResult();
+
+print_r($testResult);
+        $this->assertEquals(State::OK, $testResult->getState());
+    }
+
+    public function testConnectUriFailed()
+    {
+        $testData = new TestData([
+            'uri' => 'ssl://127.0.0.1:442',
+        ]);
+        $connectionTest = new TestCase\Network\ConnectUri($testData);
+        $testResult = $connectionTest->getTestResult();
+
+        $this->assertEquals(State::ERROR, $testResult->getState());
+    }
+}
