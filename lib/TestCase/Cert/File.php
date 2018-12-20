93df4edc22ce42aaae3c772c70328a9182f60d7d
Tim van Dijen <tvdijen@gmail.com>
2018-12-20 22:15:42 +0100

    Suppress warnings in case file does not exist

--- lib/TestCase/Cert/File.php
+++ lib/TestCase/Cert/File.php
@@ -12,7 +12,7 @@ final class File extends Data
     public function __construct(TestData $testData)
     {
         $certFile = $testData->getInputItem('certFile');
-        $certData = file_get_contents($certFile);
+        $certData = @file_get_contents($certFile);
         $testData->setInput($certData, 'certData');
 
         parent::__construct($testData);
